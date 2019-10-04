e81e5bddc0d73fa7b3165b1ba9562d62a3ee7d3c
Gabrielle Stewart <gabstew100@gmail.com>
2019-10-03 22:43:39 -0700

    added playerID's to PlayerControllers

--- Assets/Scripts/NetworkManagerLearn.cs
+++ Assets/Scripts/NetworkManagerLearn.cs
@@ -5,11 +5,14 @@ using Mirror;
 
 public class NetworkManagerLearn : NetworkManager
 {
+    private int playerID = 0;
     // Note: if there are n players, numPlayers is n-1, ex. if there is 1 player, numPlayers is 0
-        public override void OnServerAddPlayer(NetworkConnection conn, AddPlayerMessage extraMessage)
+    public override void OnServerAddPlayer(NetworkConnection conn, AddPlayerMessage extraMessage)
     {
         // Debug.Log("numPlayers connected to server: "+numPlayers);
         GameObject player = Instantiate(playerPrefab);
+        playerID += 1;
+        player.GetComponent<PlayerController>().playerName = "Player"+playerID;
         NetworkServer.AddPlayerForConnection(conn, player);
     }
 
